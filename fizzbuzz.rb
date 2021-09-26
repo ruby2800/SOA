ec8d927bb9de754387f0c79628860a907d34443b
unknown <rubyyu2800@gmail.com>
2021-09-26 14:42:53 +0800

    rubocop

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,10 @@
-## write your fizzbuzz method in this file
-# see http://en.wikipedia.org/wiki/Fizz_buzz for details on FizzBuzz game
+def fizzbuzz(size)
+  output = []
+  (1..size.to_i).each do |x|
+    a = (x % 3).zero? && (x % 5).zero? ? 'FizzBuzz' : x.to_s
+    a = 'Fizz' if (x % 3).zero? && x % 5 != 0
+    a = 'Buzz' if (x % 5).zero? && x % 3 != 0
+    block_given? ? output.push(yield a) : output.push(a)
+  end
+  output
+end
