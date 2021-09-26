1908b656eca3abb8a3fd02a67d4627d1f3351500
unknown <rubyyu2800@gmail.com>
2021-09-26 15:07:53 +0800

    fix

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,6 +5,6 @@ def fizzbuzz(size)
     a = (x % 3).zero? && (x % 5).zero? ? 'FizzBuzz' : x.to_s
     a = 'Fizz' if (x % 3).zero? && x % 5 != 0
     a = 'Buzz' if (x % 5).zero? && x % 3 != 0
-    block_given? ? output.push(yield a) : output.push(a)
+    block_given? ? yield(a.to_s) : a.to_s
   end
 end
