9c5d66ca7dadc8dc464868bdb7e5d3d71898a684
unknown <rubyyu2800@gmail.com>
2021-09-26 15:05:43 +0800

    add reason

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
+# I didn't follow robocop rules in complexity, because after I tried it, I still think this one is more easy to understand the logic
 def fizzbuzz(size)
-  output = []
-  (1..size.to_i).each do |x|
+  arr = (1..size).to_a
+  arr.map do |x|
     a = (x % 3).zero? && (x % 5).zero? ? 'FizzBuzz' : x.to_s
     a = 'Fizz' if (x % 3).zero? && x % 5 != 0
     a = 'Buzz' if (x % 5).zero? && x % 3 != 0
     block_given? ? output.push(yield a) : output.push(a)
   end
-  output
 end
